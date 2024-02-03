0114af3b32f7cc585e8d74045c53658f447c1d82
matheusz nied <matheustae@hotmail.com>
2024-02-03 18:35:41 -0300

    feat:make responsive

--- src/components/main/Contact.tsx
+++ src/components/main/Contact.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="text-white z-[50] flex flex-col justify-center items-center text-center mb-32">
+    <div className="text-white z-[50] flex flex-col justify-center items-center text-center mb-24">
       <h2 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#03C303] to-slate-50 mt-20 mb-10">
         Converse comigo
       </h2>{" "}
@@ -10,9 +10,9 @@ const Contact = () => {
         Estou sempre aberto a conhecer novas pessoas e compartilhar ideias!!
       </p>
       <p className="mt-4 text-lg">Me mande uma mensagem:</p>
-      <div className="flex flex-col sm:flex-row mt-4">
+      <div className="flex flex-col sm:flex-row mt-12">
         <a
-          className="p-3 sm:mr-6 mt-6 border rounded-sm w-36 bg-[#03C303] text-slate-950 font-medium border-transparent flex items-center justify-center hover:translate-y-[-6px] transition-all ease duration-700"
+          className="p-3 sm:mr-6 mt-6 min-w-32 border rounded-sm bg-[#03C303] text-slate-950 font-medium border-transparent flex items-center justify-center hover:translate-y-[-6px] transition-all ease duration-700"
           href="https://api.whatsapp.com/send?phone=62994395965"
           target="_blank"
           rel="noreferrer"
@@ -20,7 +20,7 @@ const Contact = () => {
           Diga olá
         </a>
         <a
-          className="p-3 sm:ml-6 mt-6 border rounded-sm w-36 text-white flex items-center justify-center hover:translate-y-[-6px] transition-all ease duration-700"
+          className="p-3 sm:ml-6 mt-6 min-w-36 border rounded-sm text-white flex items-center justify-center hover:translate-y-[-6px] transition-all ease duration-700"
           href="mailto:matheustae@hotmail.com"
           target="_blank"
           rel="noreferrer"
