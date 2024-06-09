7e268cb0d33d0436000be2ea2b11a33181c0db7c
matheusz-nied <matheustae@hotmail.com>
2024-06-09 12:14:53 -0300

    indexing google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,13 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Matheusz Nied",
   description: "Este é meu portifolio",
-  "google-site-verification":"_FTeDXvc4QC8gRP0Fy9WgXOxqXOxziiHFuO0Itc5Hf8"
 };
 
 export default function RootLayout({
@@ -19,10 +19,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="_FTeDXvc4QC8gRP0Fy9WgXOxqXOxziiHFuO0Itc5Hf8"
+        />{" "}
+      </Head>
       <body
         className={`${inter.className} bg-[#000000] overflow-y-scroll overflow-x-hidden`}
-        >
-        <StarsCanvas/>
+      >
+        <StarsCanvas />
         <Navbar />
         {children}
       </body>
